4122835eb7de9286704cd9812dcc8c699d6f8f57
倉本勇輝 <kuramotoyuki@kuramotoyuukinoMacBook-Pro.local>
2022-06-14 14:26:29 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('introduction');
 });
-
-// Route::get('/introduction', function () {
-//     return view('introduction');
-// });
